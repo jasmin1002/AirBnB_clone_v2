9cf7aabf5ff21de380c72fc9d025be700907c6e3
Jasmine <jasmineolofin@gmail.com>
2022-09-16 01:39:00 -0700

    Added modified state file

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    cities = relationship(
+        'City',
+        cascade='all, delete, delete-orphan'
+        back_populates='state'
+    )
+
+    @property
+    def cities(self):
+        return cities
