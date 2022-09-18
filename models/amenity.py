1f3b363bb1c63c64abb6c85175dc1d79e1c2c79d
Jasmine <jasmineolofin@gmail.com>
2022-09-17 21:57:28 -0700

    Updated amenity with secondary association

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -10,4 +11,8 @@ class Amenity(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     # Relationship
-    place_amenities = relationship('place_amenity')
+    place_amenities = relationship(
+        'Place',
+        secondary='place_amenity',
+        back_populates='places'
+    )
