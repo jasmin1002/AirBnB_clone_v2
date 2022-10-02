15b021e7f1b6b7261a4985360dfaf5631e84fb37
Jasmine <jasmineolofin@gmail.com>
2022-10-02 08:08:43 -0700

    Removed the comment out code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,6 @@
 [ ! -d /etc/nginx ] && sudo apt-get update && sudo apt-get -y install nginx
 [ -d /data ] || mkdir -p /data/web_static/shared /data/web_static/releases/test/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-#[ -e /data/web_static/current ] && rm /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 printf %s "server {
@@ -35,7 +34,4 @@ printf %s "server {
 		internal;
 	}
 }" > /etc/nginx/sites-available/default
-#sudo sed -i 's/server_name _;/server_name filess.tech;/' /etc/nginx/sites-available/default
-#sudo sed -i 's.\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}..' /etc/nginx/sites-available/default
-#sudo sed -i 's/server_name filess.tech;/&\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}/' /etc/nginx/sites-available/default
 sudo service nginx restart
