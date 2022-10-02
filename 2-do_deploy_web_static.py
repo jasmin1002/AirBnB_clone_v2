6d059edfa5d084709ae7dbf077a79c4e6ecb1fff
Jasmine <jasmineolofin@gmail.com>
2022-10-01 20:30:29 -0700

    Modified deploy script with return statement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,37 +19,41 @@ def do_deploy(archive_path):
 
     start = 'versions'
 
-    # Get the archive file through the file path
-    filename = os.path.relpath(archive_path, start)
+    try:
+        # Get the archive file through the file path
+        filename = os.path.relpath(archive_path, start)
 
-    # Upload the archive to /tmp/ dir web server
-    put("{}".format(archive_path), "/tmp/{}".format(filename))
+        # Upload the archive to /tmp/ dir web server
+        put("{}".format(archive_path), "/tmp/{}".format(filename))
 
-    # Cut off both the file extension and dot
-    folder = filename.split('.')[0]
+        # Cut off both the file extension and dot
+        folder = filename.split('.')[0]
 
-    # Create directory on web server
-    run("mkdir -p /data/web_static/releases/{}/".format(folder))
+        # Create directory on web server
+        run("mkdir -p /data/web_static/releases/{}/".format(folder))
 
-    # Uncompress the archive to "folder"
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-        format(filename, folder))
+        # Uncompress the archive to "folder"
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(filename, folder))
 
-    # Delete archive file from web server
-    run("rm /tmp/{}".format(filename))
+        # Delete archive file from web server
+        run("rm /tmp/{}".format(filename))
 
-    # Move content of web_static to "folder"
-    run("mv /data/web_static/releases/{0}/web_static/*".format(folder) +
-        " /data/web_static/releases/{0}/".format(folder))
+        # Move content of web_static to "folder"
+        run("mv /data/web_static/releases/{0}/web_static/*".format(folder) +
+            " /data/web_static/releases/{0}/".format(folder))
 
-    # Delete the content of web_static
-    run("rm -rf /data/web_static/releases/{}/web_static".format(folder))
+        # Delete the content of web_static
+        run("rm -rf /data/web_static/releases/{}/web_static".format(folder))
 
-    # Delete symbolic link current on web server
-    run("rm -rf /data/web_static/current")
+        # Delete symbolic link current on web server
+        run("rm -rf /data/web_static/current")
 
-    # Create new link: current and point to new version of code in "folder"
-    run("ln -s /data/web_static/releases/{}".format(folder) +
-        " /data/web_static/current")
+        # Create new link: current and point to new version of code in "folder"
+        run("ln -s /data/web_static/releases/{}".format(folder) +
+            " /data/web_static/current")
 
-    print("New version deployed!")
+        print("New version deployed!")
+        return True
+    except Exception as err:
+        return False
