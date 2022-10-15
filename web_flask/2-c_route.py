9c325515c2adeb977838210383cc05dac0d28b0d
Jasmine <jasmineolofin@gmail.com>
2022-10-14 20:24:46 -0700

    feat: Add script with endpoint route uses variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+Configure web application to listen
+on port 5000
+'''
+
+from flask import Flask
+from flask import escape
+
+#: flask: app stores instance of Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''Serves a response to entry endpoint url'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Serves a http response to 'hbnb' endpoint'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    '''
+    Serves a http response to 'c/<text>' endpoint
+
+    Args:
+        param (str): passed in data to url
+
+    Returns:
+        str: The return value
+    '''
+    #msg = '{}'.format(escape(text.replace('_', ' ')))
+    return f"c {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
