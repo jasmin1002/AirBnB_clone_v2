67a8256bf5e1a5c9b4808079ba441bcc7717eefa
Jasmine <jasmineolofin@gmail.com>
2022-10-16 04:04:14 -0700

    fix(pycodestyle): Add blank line to suit pycodesty

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
 
